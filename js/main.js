07001f9a23f18e9ace3ca53899aaa22ab9d02db8
Ashok <bayes.ashok@gmail.com>
2023-10-15 20:24:18 +0545

    added remember me

--- js/main.js
+++ js/main.js
@@ -11,3 +11,37 @@ if (typed) {
     backDelay: 2000
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+  const emailInput = document.getElementById("inputField1");
+  const passwordInput = document.getElementById("inputField2");
+  const rememberMeCheckbox = document.getElementById("rememberMe");
+
+  // Check if "rememberMe" data is stored in local storage and populate the fields if it exists
+  if (localStorage.getItem("rememberMeData")) {
+    const data = JSON.parse(localStorage.getItem("rememberMeData"));
+    emailInput.value = data.email;
+    passwordInput.value = data.password;
+    rememberMeCheckbox.checked = true;
+  }
+
+  // Add a click event listener to the "Login" button
+  document.getElementById("loginButton").addEventListener("click", function () {
+    // Get the user input
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    // Check if "Remember Me" is checked and save data to local storage
+    if (rememberMeCheckbox.checked) {
+      const rememberMeData = JSON.stringify({ email, password });
+      localStorage.setItem("rememberMeData", rememberMeData);
+    } else {
+      // If "Remember Me" is not checked, remove the data from local storage
+      localStorage.removeItem("rememberMeData");
+    }
+
+    // Perform other login actions as needed
+    // Example: loginForm.submit();
+  });
+});

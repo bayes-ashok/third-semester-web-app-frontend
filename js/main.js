6ad604e7519820fcdfe650acbe20433f8ca4799f
Ashok <bayes.ashok@gmail.com>
2023-10-15 20:53:31 +0545

    code optimized in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Animation in the login page
+//animation in the login page for sajilo city
 const typed = document.querySelector('.typed')
 if (typed) {
   let typed_strings = typed.getAttribute('data-typed-items')
@@ -12,6 +12,7 @@ if (typed) {
   });
 }
 
+//email, pw validation, remember me in login page
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("loginForm");
   const emailInput = document.getElementById("inputField1");
@@ -20,13 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const emailWarning = document.getElementById("emailWarning");
   const passwordWarning = document.getElementById("passwordWarning");
 
-  // Function to validate an email address using a regular expression
+  //email address validation
   function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
 
-  // Add an input event listener to the email field for real-time validation
+  //input event listener to the email field for real-time validation
   emailInput.addEventListener("input", function () {
     const email = emailInput.value;
     if (!isValidEmail(email)) {
@@ -37,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Add an input event listener to the password field for real-time validation
+  // input event listener to the pw field for real-time validation
   passwordInput.addEventListener("input", function () {
     const password = passwordInput.value;
     if (!password) {
@@ -48,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Add a blur event listener to the email and password fields
+  // blur event listener to the email and password fields
   emailInput.addEventListener("blur", function () {
     if (!isValidEmail(emailInput.value)) {
       emailWarning.innerText = "Please enter a valid email address.";
@@ -67,9 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Add a click event listener to the "Login" button
+  // click event listener to the "Login" button
   document.getElementById("loginButton").addEventListener("click", function () {
-    // Get the user input
     const email = emailInput.value;
     const password = passwordInput.value;
 
@@ -82,14 +82,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Check if a password is provided
     if (!password) {
-      // Display a warning if no password is provided
       passwordWarning.innerText = "Please enter a password.";
       passwordWarning.style.color = "red";
       return;
     }
 
-    // If both email and a password (any value) are provided, you can perform the login action
-    // Example: loginForm.submit();
+    // If both email and a password (any value) are provided, we can perform the login action
+    // for now redirect to fb
     window.location.href = "https://www.fb.com";
   });
 });

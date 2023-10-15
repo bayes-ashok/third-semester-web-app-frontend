bf852d9980cf9656bcf31839b7dc60317de764f2
Ashok <bayes.ashok@gmail.com>
2023-10-15 12:41:04 +0545

    img added, working on modal

--- js/main.js
+++ js/main.js
@@ -11,7 +11,34 @@ if (typed) {
     backDelay: 2000
   });
 }
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 
 // page switch betn login and signup
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const loginForm = document.getElementById("loginForm");
+//   const registerForm = document.getElementById("registerForm");
+//   const showLoginFormLink = document.getElementById("showLoginForm");
+//   const showRegisterFormLink = document.getElementById("showRegisterForm");
+
+//   // Function to show the login form and hide the registration form
+//   function showLoginForm() {
+//     loginForm.style.display = "block";
+//     registerForm.style.display = "none";
+//   }
+
+//   // Function to show the registration form and hide the login form
+//   function showRegisterForm() {
+//     loginForm.style.display = "none";
+//     registerForm.style.display = "block";
+//   }
+
+//   // Add click event listeners to the "Login" and "Register" links
+//   showLoginFormLink.addEventListener("click", showLoginForm);
+//   showRegisterFormLink.addEventListener("click", showRegisterForm);
+// });
+
 
 
